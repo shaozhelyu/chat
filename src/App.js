70988355f772a314443f91221628d532f4dcebbc
shaozhelyu <shaozhe@seas.upenn.edu>
2020-11-17 20:39:05 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProfileLoad from './component/ProfileLoad.js';
+class App extends React.PureComponent {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path="/" render={() => <ProfileLoad />} />
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
